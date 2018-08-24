077f54a33fadd893d3e04819e974787ee2adba32
MarconiS <sergio.marconi@weecology.org>
2018-08-24 15:09:52 -0400

    up

--- src/get_values_from_netcdf.R
+++ src/get_values_from_netcdf.R
@@ -24,7 +24,7 @@ get_values_from_netcdf <- function(clim_dat = NULL, path_daymet = "/Volumes/rast
     ext <- raster::extract(one_year_tile,data_to_extract_from)
     out <- list()
     out[[mth]] <- tmp
-    for(mth in 1:2){
+    for(mth in 1:12){
       tmp <- ext[,month_vect == mth]
       if(meta_daym[1] %in% c("dayl", "srad", "swe", "tmax", "tmin", "vp")){
         tmp <- apply(tmp, 1, mean, na.rm=T)
